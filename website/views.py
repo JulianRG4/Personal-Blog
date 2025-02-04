695450c2c0bca1ddf1c4d6614dae64dddd1acf8f
julian gonzalez <juliang4209@gmail.com>
2025-02-04 08:35:22 -0800

    logic so logged in users only see home and logout and users logged out see login, signup, and home. also started adding code for POST

--- website/views.py
+++ website/views.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 @views.route('/home')
+@login_required
 def home():
-    return render_template("home.html", name="joe")
+    return render_template("home.html", user=current_user)
+
+@views.route("/create-post", methods=["GET", "POST"])
+@login_required
+def create_post():
+    return render_template("create_post.html", user=current_user)
